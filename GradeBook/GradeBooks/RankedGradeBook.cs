b2a65c4ac5fdb048ffdf2bb879bba753ad20e599
kacpito <kacper.jewula5@gmail.com>
2023-03-17 22:35:22 +0100

    created and updated RankedGradeBook

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using GradeBook.Enums;
+
+namespace GradeBook.GradeBooks
+{
+    public class RankedGradeBook : BaseGradeBook
+    {
+        public RankedGradeBook(string name) : base(name)
+        {
+            Name = name;
+            Type = GradeBookType.Ranked;
+        }
+    }
+}
