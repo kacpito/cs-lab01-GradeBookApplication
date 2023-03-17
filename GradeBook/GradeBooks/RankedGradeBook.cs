290189b6958c642c4573b45bdffce8781fd5ba11
kacpito <kacper.jewula5@gmail.com>
2023-03-17 23:13:33 +0100

    overrided RankedGradeBook's CalculateStatistics

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Xml.Linq;
 using GradeBook.Enums;
 
 namespace GradeBook.GradeBooks
@@ -11,6 +12,22 @@ namespace GradeBook.GradeBooks
         {
             Name = name;
             Type = GradeBookType.Ranked;
+        }
+        /*
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+                throw new ArgumentException("Not enough students!");
+
+            int N = Students.Count / 5;
+        }
+        */
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+            else
+                base.CalculateStatistics();
         }
     }
 }
